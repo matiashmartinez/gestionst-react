163b99474270ba57cc93ceb90401d1305b0aa27e
Matías Hernán Martinez <mati8910@hotmail.com>
2024-12-01 22:16:30 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {}, // Asegura que no hay problemas de compatibilidad con las variables
+  },
   outDir: "dist", // Directorio de salida
   base: '/',
+   'process.env': {},
 })
